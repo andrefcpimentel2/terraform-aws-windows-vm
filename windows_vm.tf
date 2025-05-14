57d8e2690d47bc7c5dd0e11fb25ba155a23138e7
andrefcpimentel2 <andre@hashicorp.comp>
2025-05-14 14:44:48 +0200

    fixed agent template

--- windows_vm.tf
+++ windows_vm.tf
@@ -21,15 +21,15 @@ Rename-Computer -NewName "${var.vm_name}" -Force;
 Install-WindowsFeature -name Web-Server -IncludeManagementTools;
 
 New-Item -ItemType Directory -Path "c:\vault"
+New-Item -Path "c:\vault" -Name "roleid" -ItemType "File" -Value "${var.role_id}"
+New-Item -Path "c:\vault" -Name "rolesecret" -ItemType "File" -Value "${var.role_secret}"
 
 Invoke-WebRequest ${var.vault_binary_url} -OutFile c:\vault\vault.zip
 Expand-Archive -Path c:\vault\vault.zip -DestinationPath c:\vault
 
 @"
-pid_file = "./pidfile"
-
 vault {
-  address = "https://${var.vault_addr}:8200"
+  address = "${var.vault_addr}"
   namespace = "${var.vault_namespace}"
 }
 auto_auth {
@@ -37,37 +37,19 @@ auto_auth {
     type      = "approle"
 
     config = {
-      role_id_file_path = "${var.role_id}"
-      secret_id_file_path = "${var.role_secret}"
+      role_id_file_path = "c:\vault\roleid"
+      secret_id_file_path = "c:\vault\rolesecret"
       remove_secret_id_file_after_reading = false
     }
   }
-
-  sink {
-    type = "file"
-    wrap_ttl = "30m"
-    config = {
-      path = "sink_file_wrapped_1.txt"
-    }
-  }
-
-template_config {
-  static_secret_render_interval = "5m"
-  exit_on_retry_failure         = true
-  max_connections_per_host      = 10
-}
-
-env_template "CERT" {
-  contents             = "{{- with pkiCert "pki_int/issue/example-dot-com" "ttl=24h" "common_name=foo.example.com" -}}{{ .Cert }}{{ .CA }}{{ if .Key }}{{ .Key  | writeToFile "C:\\Program Files\Vault\\Data\\certificate.key" "" "" "" }}{{ end }}"
-  error_on_missing_key = true
 }
 
 
 template {
-source = "C:\Vault\agent\wincert.tpl"
-destination = "C:\Vault\Data\certificate.crt"
-command = "Set-Location -Path cert:\CurrentUser\\My ; Import-Certificate -Filepath 'C:\Vault\Data\certificate.crt"
+  contents             = "{{- with pkiCert \"pki_int/issue/example-dot-com\" \"ttl=24h\" \"common_name=foo.example.com\" -}}{{ .Cert }}{{- end }}"
+  destination  = "c:\vault\cert.crt"
 }
+
 "@ | tee "C:\vault\agent-config.hcl" 
 
 sc.exe create VaultAgent binPath="C:\vault\vault.exe agent -config=C:\vault\agent-config.hcl" displayName="Vault Agent" start=auto
