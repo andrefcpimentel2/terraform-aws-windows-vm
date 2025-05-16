127e69be81bd400b041b1223950754db1ea956a7
andrefcpimentel2 <andre@hashicorp.comp>
2025-05-16 11:55:29 +0200

    count output

--- outputs.tf
+++ outputs.tf
@@ -3,15 +3,15 @@ output "vm_instance_name" {
 }
 
 output "vm_windows_server_instance_public_dns" {
-  value = aws_instance.windows-demo.public_dns
+  value = aws_instance.windows-demo.*.public_dns
 }
 
 output "vm_instance_public_ip" {
-  value = aws_eip.windows-demo.public_ip
+  value = aws_eip.windows-demo.*.public_ip
 }
 
 output "vm_instance_private_ip" {
-  value = aws_instance.windows-demo.private_ip
+  value = aws_instance.windows-demo.*.private_ip
 }
 
 output "vm_instance_username" {
@@ -19,5 +19,5 @@ output "vm_instance_username" {
 }
 
 output "vm_password_data" {
-  value = "${rsadecrypt(aws_instance.windows-demo.password_data, nonsensitive(tls_private_key.windows-demo.private_key_pem))}"
+  value = "${rsadecrypt(aws_instance.windows-demo.*.password_data, nonsensitive(tls_private_key.windows-demo.*.private_key_pem))}"
 }
